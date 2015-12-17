1861787e32e56980971ffe2c0d366407764b82b2
asdf <roman.meier@students.fhnw.ch>
2015-12-17 19:49:22 +0100

    Initial commit

---
+++ src/digraph/Digraph.java
@@ -0,0 +1,41 @@
+package digraph;
+
+import java.util.LinkedList;
+
+/**
+ * 
+ * @author Roman
+ *
+ * @param <V>
+ * @param <E>
+ */
+public class Digraph<V, E> {
+
+	/**
+	 * Stores Verts that are connected to each other.
+	 */
+	private LinkedList<V>[] vertex;
+	
+	/**
+	 * m_size is defined by the number of Edges.
+	 */
+	private int m_size;
+	
+	/**
+	 * Constructor ...
+	 * 
+	 * @param size: 
+	 */
+	public Digraph(int size){
+		
+	}
+	
+	/**
+	 * 
+	 * @author Roman
+	 *
+	 */
+	static class Edge {
+		
+	}
+}
